525be8683c8992c5d9b06b57deae576897c3ab3a
Camilo <c.acevedo1993@gmail.com>
2025-02-23 15:13:33 -0500

    fix build process

--- src/app/api/blogs/[id]/delete/route.ts
+++ src/app/api/blogs/[id]/delete/route.ts
@@ -1,31 +1,43 @@
+import { NextRequest, NextResponse } from "next/server";
 import BlogPost from "@/models/BlogPost";
-import { NextResponse } from "next/server";
 import connect from "@/lib/db";
 
 export async function PATCH(
-  req: Request,
-  { params }: { params: { id: string } }
+  req: NextRequest,
+  { params }: { params: { id: string } } // <-- Parámetros correctos
 ) {
   await connect();
 
   try {
     const { isDeleted } = await req.json();
 
-    if (!params.id) {
-      return NextResponse.json({ error: "ID de usuario no proporcionado" }, { status: 400 });
+    if (!params?.id) {
+      return NextResponse.json(
+        { error: "ID de usuario no proporcionado" },
+        { status: 400 }
+      );
     }
 
     const blogPost = await BlogPost.findById(params.id);
     if (!blogPost) {
-      return NextResponse.json({ error: "Usuario no encontrado" }, { status: 404 });
+      return NextResponse.json(
+        { error: "Usuario no encontrado" },
+        { status: 404 }
+      );
     }
 
     blogPost.isDeleted = isDeleted;
     await blogPost.save();
 
-    return NextResponse.json({ message: "Estado actualizado exitosamente", blogPost });
+    return NextResponse.json({
+      message: "Estado actualizado exitosamente",
+      blogPost,
+    });
   } catch (error) {
     console.error("Error al actualizar el estado del usuario:", error);
-    return NextResponse.json({ error: "Error al actualizar el estado del usuario" }, { status: 500 });
+    return NextResponse.json(
+      { error: "Error al actualizar el estado del usuario" },
+      { status: 500 }
+    );
   }
 }
